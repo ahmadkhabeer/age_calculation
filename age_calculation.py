2a88976c4ae363655ccea13b35122f6e2381b8cc
ahmadkhabeer <ahmadkhabeer@outlook.com>
2023-09-13 22:49:40 +0200

    Initial commit

---
+++ age_calculation.py
@@ -0,0 +1,50 @@
+"""
+    Import date and datetime classes from datetime module.
+    Import isleap method from calendar module.
+"""
+from datetime import date, datetime
+from calendar import isleap
+
+try:
+    def calc_my_age(user_birthdate):
+        """
+            Returns the age of the user
+            in years, months, weeks, and days.    
+        """
+        year_months = {0:31, 1:31, 2:28, 3:31, 4:30, 5:31, 6:30, \
+            7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+        today = date.today()
+        user_birthdate = datetime.strptime(user_birthdate, "%d-%m-%Y")
+
+        years = today.year - user_birthdate.year
+        months = today.month - user_birthdate.month
+        weeks = 0
+        days = today.day - user_birthdate.day
+
+        if months < 0:
+            years -= 1
+            months += 12
+
+        if days < 0:
+            months -=1
+            days += year_months[user_birthdate.month]
+
+            if (isleap(today.year) and today.month >= 2 and today.day >= 29):
+                days += 1
+            if (isleap(user_birthdate.year) and user_birthdate.month <= 2 and user_birthdate <= 29):
+                days += 1
+
+        if days > 6:
+            weeks = days//7
+            days = days%7
+
+        print('You have {}{}{}{}'.format(str(years) + " year(s)" if years > 0 else "\b", \
+            ", " + str(months) + " month(s)" if months > 0 else "\b\b", \
+            ", " + str(weeks) + " week(s)" if weeks > 0 else "\b\b", \
+            ", " + str(days) + " day(s)" if days > 0 else "\b\b"))
+
+    birthdate = input('Enter your bithdate as "d-m-Y: ')
+
+    calc_my_age(birthdate)
+except ValueError:
+    print('You should enter your birthdate in dd-mm-yyyy format.')
